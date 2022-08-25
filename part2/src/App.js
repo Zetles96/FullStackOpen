5a0d752c9481417d8cb0386300e345393c05ba2e
Mads Legard Nielsen <mln@logos.dk>
2022-08-25 14:38:01 +0200

    done 3.1-3.6

--- part2/src/App.js
+++ part2/src/App.js
@@ -4,157 +4,166 @@ import Notification from "./components/Notification";
 import peopleService from "./Services/people";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
-  const [newName, setNewName] = useState("");
-  const [newNumber, setNewNumber] = useState("");
-  const [personSearch, setPersonSearch] = useState("");
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [noteficationMessage, setNoteficationMessage] = useState(null);
-  const hook = () => {
-    peopleService.getAll().then((all) => {
-      setPersons(all);
-    });
-  };
-
-  useEffect(hook, []);
-
-  // // componentDidMount
-  // useEffect(() => {}, []);
-
-  // // Everything input changes. run this effect
-  // useEffect(() => {}, [input]);
-
-  // // When the component is removed from the dom
-  // useEffect(() => {
-  //   return () => {};
-  // }, []);
-
-  const addPerson = (event) => {
-    event.preventDefault();
-    const personObject = {
-      name: newName,
-      number: newNumber,
-      id: persons[persons.length - 1].id + 1,
+    const [persons, setPersons] = useState([]);
+    const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
+    const [personSearch, setPersonSearch] = useState("");
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [noteficationMessage, setNoteficationMessage] = useState(null);
+    const hook = () => {
+        peopleService.getAll().then((all) => {
+            setPersons(all);
+        });
     };
 
-    var found = persons.some(
-      (p) => p.name.toLowerCase() === newName.toLowerCase()
-    );
-    if (found) {
-      if (
-        window.confirm(
-          `${newName} is already added to the phonebook, do you want to update the number?`
-        )
-      ) {
-        const pleb = persons.filter(
-          (person) => person.name.toLowerCase() === newName.toLowerCase()
-        );
+    useEffect(hook, []);
 
-        const updatedpleb = {
-          number: newNumber,
-          name: pleb[0].name,
+    // // componentDidMount
+    // useEffect(() => {}, []);
+
+    // // Everything input changes. run this effect
+    // useEffect(() => {}, [input]);
+
+    // // When the component is removed from the dom
+    // useEffect(() => {
+    //   return () => {};
+    // }, []);
+
+    const addPerson = (event) => {
+        event.preventDefault();
+        const personObject = {
+            name: newName,
+            number: newNumber,
+            id: persons[persons.length - 1].id + 1,
         };
-        peopleService
-          .update(pleb[0].id, updatedpleb)
-          .then((updatedPeople) => {
-            const copyPersons = [...persons];
-            const index = copyPersons.findIndex(
-              (thing) => thing.id === pleb[0].id
-            );
-            copyPersons[index] = updatedPeople;
-            setPersons(copyPersons);
+
+        let found = persons.some(
+            (p) => p.name.toLowerCase() === newName.toLowerCase()
+        );
+        if (found) {
+            if (
+                window.confirm(
+                    `${newName} is already added to the phonebook, do you want to update the number?`
+                )
+            ) {
+                const pleb = persons.filter(
+                    (person) =>
+                        person.name.toLowerCase() === newName.toLowerCase()
+                );
+
+                const updatedpleb = {
+                    number: newNumber,
+                    name: pleb[0].name,
+                };
+                peopleService
+                    .update(pleb[0].id, updatedpleb)
+                    .then((updatedPeople) => {
+                        const copyPersons = [...persons];
+                        const index = copyPersons.findIndex(
+                            (thing) => thing.id === pleb[0].id
+                        );
+                        copyPersons[index] = updatedPeople;
+                        setPersons(copyPersons);
+                        setNewName("");
+                        setNewNumber("");
+                        setNoteficationMessage(
+                            `${personObject.name}s number was succesfully changed`
+                        );
+                        setTimeout(() => {
+                            setNoteficationMessage(null);
+                        }, 5000);
+                    })
+                    .catch((error) => {
+                        setErrorMessage(
+                            `${personObject.name} was already removed from the server`
+                        );
+                    });
+            }
+            return;
+        }
+        peopleService.create(personObject).then((updatedPeople) => {
+            setPersons(persons.concat(updatedPeople));
             setNewName("");
             setNewNumber("");
             setNoteficationMessage(
-              `${personObject.name}s number was succesfully changed`
+                `${personObject.name} was succesfully added`
             );
             setTimeout(() => {
-              setNoteficationMessage(null);
+                setNoteficationMessage(null);
             }, 5000);
-          })
-          .catch((error) => {
-            setErrorMessage(
-              `${personObject.name} was already removed from the server`
-            );
-          });
-      }
-      return;
-    }
-    peopleService.create(personObject).then((updatedPeople) => {
-      setPersons(persons.concat(updatedPeople));
-      setNewName("");
-      setNewNumber("");
-      setNoteficationMessage(`${personObject.name} was succesfully added`);
-      setTimeout(() => {
-        setNoteficationMessage(null);
-      }, 5000);
-    });
-  };
+        });
+    };
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
+    const handleNameChange = (event) => {
+        setNewName(event.target.value);
+    };
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value);
+    };
 
-  const handlePersonSearChange = (event) => {
-    setPersonSearch(event.target.value);
-  };
+    const handlePersonSearChange = (event) => {
+        setPersonSearch(event.target.value);
+    };
 
-  const personsToShow = persons.filter((person) =>
-    person.name.toLowerCase().includes(personSearch.toLowerCase())
-  );
+    const personsToShow = persons.filter((person) =>
+        person.name.toLowerCase().includes(personSearch.toLowerCase())
+    );
 
-  const handleDeleteButtonClick = (personId) => {
-    const person = persons.find((x) => x.id === personId);
-    if (
-      person &&
-      window.confirm(`do you really want do delete ${person.name}`)
-    ) {
-      peopleService.deleteByid(personId).then(() => {
-        setPersons(persons.filter((person) => person.id !== personId));
-      });
-    }
-  };
+    const handleDeleteButtonClick = (personId) => {
+        const person = persons.find((x) => x.id === personId);
+        if (
+            person &&
+            window.confirm(`do you really want do delete ${person.name}`)
+        ) {
+            peopleService.deleteByid(personId).then(() => {
+                setPersons(persons.filter((person) => person.id !== personId));
+            });
+        }
+    };
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Notification.Notification
-        message={noteficationMessage}
-      ></Notification.Notification>
-      <Notification.ErrorNotification
-        message={errorMessage}
-      ></Notification.ErrorNotification>
-      <form>
+    return (
         <div>
-          filter shown with:
-          <input value={personSearch} onChange={handlePersonSearChange}></input>
+            <h2>Phonebook</h2>
+            <Notification.Notification
+                message={noteficationMessage}
+            ></Notification.Notification>
+            <Notification.ErrorNotification
+                message={errorMessage}
+            ></Notification.ErrorNotification>
+            <form>
+                <div>
+                    filter shown with:
+                    <input
+                        value={personSearch}
+                        onChange={handlePersonSearChange}
+                    ></input>
+                </div>
+            </form>
+            <form onSubmit={addPerson}>
+                <h2>Add new contact</h2>
+                <div>
+                    name:
+                    <input value={newName} onChange={handleNameChange}></input>
+                </div>
+                <div>
+                    number:
+                    <input
+                        value={newNumber}
+                        onChange={handleNumberChange}
+                    ></input>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <Persons
+                persons={personsToShow}
+                handleDeleteButtonClick={handleDeleteButtonClick}
+            ></Persons>
         </div>
-      </form>
-      <form onSubmit={addPerson}>
-        <h2>Add new contact</h2>
-        <div>
-          name:
-          <input value={newName} onChange={handleNameChange}></input>
-        </div>
-        <div>
-          number:
-          <input value={newNumber} onChange={handleNumberChange}></input>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <Persons
-        persons={personsToShow}
-        handleDeleteButtonClick={handleDeleteButtonClick}
-      ></Persons>
-    </div>
-  );
+    );
 };
 
 export default App;
--- part3/index.js
+++ part3/index.js
@@ -1,44 +1,111 @@
 const { request, response } = require("express");
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
 
 let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456",
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345",
+    },
+    {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+    },
 ];
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+    response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find((person) => person.id === id);
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+    const id = Number(request.params.id);
+    notes = persons.filter((person) => person.id !== id);
+
+    response.status(204).end();
+});
+
+const randomIntFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const generateId = () => {
+    const id = randomIntFromInterval(1, 99999);
+    const idfound = persons.find((person) => person.id === id);
+    if (idfound) {
+        generateId();
+    }
+    return id;
+};
+
+app.post("/api/persons", (request, response) => {
+    const body = request.body;
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: "content missing",
+        });
+    }
+
+    let found = persons.some(
+        (p) => p.name.toLowerCase() === body.name.toLowerCase()
+    );
+
+    if (found) {
+        return response.status(400).json({
+            error: "name must be unique",
+        });
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    };
+
+    persons = persons.concat(person);
+
+    response.json(person);
 });
 
 app.get("/info", (request, response) => {
-  const date = new Date();
-  response.send(
-    `<p> Phonebook has info for ${persons.length} people</p> <p>${date} </p>`
-  );
+    const date = new Date();
+    response.send(
+        `<p> Phonebook has info for ${persons.length} people</p> <p>${date} </p>`
+    );
 });
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
